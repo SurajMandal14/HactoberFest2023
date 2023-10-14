3b2a572fec302a09735039dc72808ee0bed18809
SurajMandal14 <122726373+SurajMandal14@users.noreply.github.com>
2023-10-14 16:31:10 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,15 @@
+# Get user input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: ")
+
+# Perform arithmetic operations
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+# Display the results
+print(f"Sum: {sum}")
+print(f"Difference: {difference}")
+print(f"Product: {product}")
+print(f"Quotient: {quotient}")
